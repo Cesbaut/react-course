e8c62ec5c6869a89abf9111d62ee31f7d1a78d18
Cesar Bautista <bautistac.cesar.p8@gmail.com>
2025-09-27 20:15:48 -0600

    Archivos deployed

--- 01-fundamentos/src/App.jsx
+++ 01-fundamentos/src/App.jsx
@@ -4,16 +4,29 @@ import "./App.css"
 
 const App = () => {
 
-  const state = useState(20)
+  //const state = useState(20)
+  const [count, setCount] = useState(10)
 
-  console.log(state)
+  // setTimeout(() => {
+  //   setCount(count + 1)
+  // }, 1000);
 
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+  const handleSubtract = () => {
+    setCount(count - 1)
+  }
+  const handleReset = () => {
+    setCount(10)
+  }
+  //Fragment <> </>
   return (
   <div className="container">
-    <h1>Counter : 0</h1>
-    <button>+</button>
-    <button>Reset</button>
-    <button>-</button>
+    <h1>Counter : { count }</h1>
+    <button onClick={handleAdd}>+</button>
+    <button onClick={handleReset}>Reset</button>
+    <button onClick={handleSubtract}>-</button>
   </div>
   )
 }
